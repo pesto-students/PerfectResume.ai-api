dc35c994195d3416daa04cfca4aa29e9e373c112
Siddharth Choudhary <siddharthchoudhary.mail@gmail.com>
2023-12-10 15:28:23 +0530

    Ad

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    origin: true,
-  });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: true });
   await app.listen(3000);
 }
 bootstrap();
